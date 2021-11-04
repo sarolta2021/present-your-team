399f8d69dd4b555e3d8d5bc6406b50ac9cf83593
10Cdegree <armin.szathmari@outlook.com>
2021-11-04 16:33:57 +0100

    Data appears correctly

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 function loadFirstTeam() {
     fetch('fantasticfour.json')
         .then(r => r.json())
-        .then(displayPeoples);
+        .then(displayFirstTeam);
 }
 
-function displayPeoples(peoples) {
+function displayFirstTeam(peoples) {
     displayPeople(peoples[0]);
+    displayPeople(peoples[1]);
+    displayPeople(peoples[2]);
+    displayPeople(peoples[3]);
 }
 
 function displayPeople(people) {
     let name = document.createElement("h1");
     let email = document.createElement("div");
     name.innerText = people.name;
+    email.innerText = people.email;
     let box = document.createElement("div");
     box.append(name, email);
-    document.getElementById("people").append(box);
+    document.getElementById("peoples").append(box);
 }
 
 window.addEventListener('load', loadFirstTeam);
