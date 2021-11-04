47fe4e1afee4071845a51278b198fabf5d94a47c
10Cdegree <armin.szathmari@outlook.com>
2021-11-04 15:59:36 +0100

    More cleanup

--- index.js
+++ index.js
@@ -11,5 +11,13 @@ function loadFirstTeam() {
 //}
 
 function displayData(firstteam) {
-
+    let name = document.createElement('h1');
+    let email = document.createElement('div');
+    let box = document.createElement("div");
+    name.innerText = personname.name;
+    email.innerText = personmail.email;
+    box.append(name, email);
+    document.getElementById("teams").append(box);
 }
+
+window.addEventListener('load', loadFirstTeam);
